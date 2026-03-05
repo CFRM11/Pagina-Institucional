e3cb63f8925f5e28245d410a360ffd3699c32494
Christian Rodriguez <yochristian804@gmail.com>
2026-03-04 18:25:32 -0600

    Ajustes componentes CSS Responsive

--- Js/main.js
+++ Js/main.js
@@ -8,11 +8,14 @@ function initNavbarEffects() {
     const navbar = document.querySelector(".navbar-wrapper");
     const heroe = document.querySelector(".heroe");
 
-    // Crear elemento fantasma
     const placeholder = document.createElement("div");
     placeholder.style.height = navbar.offsetHeight + "px";
     placeholder.style.display = "none";
     navbar.parentNode.insertBefore(placeholder, navbar.nextSibling);
+
+    function updatePlaceHolderHeight() {
+        placeholder.style.height = navbar.offsetHeight + "px";
+    }
 
     window.addEventListener("scroll", function () {
         const heroeHeight = heroe.offsetHeight;
@@ -24,6 +27,13 @@ function initNavbarEffects() {
             navbar.classList.remove("sticky");
         }
     });
+
+    window.addEventListener("resize", function () {
+        updatePlaceHolderHeight();
+    }
+    );
+    updatePlaceHolderHeight();
+
 }
 
 
